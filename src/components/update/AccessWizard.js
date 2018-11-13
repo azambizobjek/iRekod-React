5f72257da488a1310193bc94560a628fae4d5f98
Kardiwangsa <fariz@bizobjek.com>
2018-11-13 12:42:37 +0800

    Update src/components/update/AccessWizard.js

--- src/components/update/AccessWizard.js
+++ src/components/update/AccessWizard.js
@@ -97,7 +97,7 @@ class accessWizard extends Component {
             const accRmv = remove.map(itm=>({value: itm.stakeholder_id, label:decodeURIComponent(itm.stakeholder_name), remove: true}))
             //  console.log(accView)
 
-            const accMod = aclMod.map(itm=>({value: itm.stakeholder_id, label:decodeURIComponent(itm.stakeholder_name), modify_access: true}))
+            const accMod = aclMod.map(itm=>({value: itm.stakeholder_id, label:decodeURIComponent(itm.stakeholder_name)}))
             //  console.log(accView)
             
             this.setState({ 
@@ -111,7 +111,7 @@ class accessWizard extends Component {
 
     }
 
-    
+
 
 
 
