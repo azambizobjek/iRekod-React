805ecadd5a53dadcc3ff579d89dfb040324c1754
Kardiwangsa <fariz@bizobjek.com>
2018-11-19 15:37:55 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,10 +33,8 @@ class Home extends Component {
         'addStakeholder' : addStakeholder,                
         'index' : index,
         'view': ViewDetail,
-        'edit': UpdateDetail,
+        'edit': UpdateDetail,       
         
-        
-
     }
      
 
