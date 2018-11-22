43615d7854056b1c3e60b75abc9079d758e74593
Kardiwangsa <fariz@bizobjek.com>
2018-11-22 18:06:25 +0800

    Update appConfig.js

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,3 +1,4 @@
-const biorisUrl = 'http://localhost:8000/api'
-
+//const biorisUrl = 'http://localhost:8000/api' //local bo-gw
+const biorisUrl = 'http://192.168.0.110:8000/api' //dev server bo-gw
 export{biorisUrl}
+
