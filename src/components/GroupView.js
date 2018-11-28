dfc0ce86699b0dae0d45b32d13ea2e919a0f3e91
Kardiwangsa <fariz@bizobjek.com>
2018-11-28 10:16:32 +0800

    Update GroupView.js

--- src/components/GroupView.js
+++ src/components/GroupView.js
@@ -9,14 +9,12 @@ export default function GroupView({stkhId,fullName,typeName,setActivePage,stakeh
     // console.log(e.target.data-pagename, e.target.id)
   }
 
-  return (
-    <div>
+  return (     
       <div id={stkhId} name={fullName} className="mt-2">
             <div className={"col-lg-12 col-md-12 col-sm-12 "} >
                 <img src={require('../img/StakeType/'+ typeName +'.svg')} alt="person" className="img-list mx-auto d-block" />              
                 <p className={"card-title text-truncate ml-2 mb-2 btn btn-link"} data-id={stkhId} onClick={pageBtn}>{decodeURIComponent(fullName)}</p>                                        
             </div>
-        </div> 
-    </div>
+        </div>     
   )
 }
