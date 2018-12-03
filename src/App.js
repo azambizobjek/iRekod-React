50fefdde1745eae5c7d3215c414e7ff39f933ba0
Kardiwangsa <fariz@bizobjek.com>
2018-12-03 15:32:29 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 import {Provider} from 'react-redux'
 import store from './store'
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import './css/seaTheme.css';
-import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './css/seaTheme.css'
+import './css/style.css'
 
-import ViewPort from '../src/components/ViewPort';
+import ViewPort from '../src/components/ViewPort'
 
+ 
 class App extends Component {
   render() {
-    return (
-      <Provider store={store}>
-      <ViewPort></ViewPort>
-      </Provider>
+    return (     
+        <Provider store={store}>
+          <ViewPort></ViewPort>
+        </Provider> 
     );
   }
 }
