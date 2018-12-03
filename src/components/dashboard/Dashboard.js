1c68de0df700ce46cd03bfc733d17c0070ddd9a6
Kardiwangsa <fariz@bizobjek.com>
2018-12-03 10:55:24 +0800

    Create Dashboard.js

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+import React, { Component,Fragment } from 'react'
+import Card from '../dashboard/Card'  
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="breadcrumb">
+          <h4 className="modal-title">DASHBOARD</h4>          
+        </div>
+
+       
+            <Card
+            
+            />
+
+
+          
+        
+  
+      </Fragment>
+    )
+  }
+}
