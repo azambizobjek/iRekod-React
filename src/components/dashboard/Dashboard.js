48fece67238859b7db61d96b640b1a265ec526fc
Kardiwangsa <fariz@bizobjek.com>
2018-12-03 18:09:30 +0800

    Update Dashboard.js

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,31 @@
 import React, { Component,Fragment } from 'react'
 import Card from '../dashboard/Card'  
+import {setActivePage,setPageTitle} from '../../actions/layoutInitAction'
+import {setStakehType,setStakehNumb} from '../../actions/stakehTypeAction'
 
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 
 class Dashboard extends Component {
 
+  setActivePage=(value)=>{
+   console.log(value)
+    // const {user:{stakeholder_id:bId,bio_access_id:idAccess}} = this.props.session
+
+    // // this.props.setActivePage(e.target.getAttribute('data-pagename'))
+    // // this.props.setPageTitle(e.target.getAttribute('data-pagetitle'))
+    // // this.props.setStakehNumb(e.target.getAttribute('data-id'))
+    // // console.log(e.target.getAttribute('data-pageTitle'))     
+
+    // const stakehObj={
+    //   stakeholder_id:bId,
+    //   bio_access_id:idAccess,
+    //   action:'ITEM_LIST_TYPE',
+    //   stakeh_type: parseInt(e.target.getAttribute('data-id')),
+    // }
+
+    // this.props.setStakehType(stakehObj) 
+  }
 
   render() {
     
@@ -14,23 +34,20 @@ class Dashboard extends Component {
       <Fragment>
          <div className="breadcrumb-holder">
             <div className="container-fluid">        
-              <div className="breadcrumb"> 
-          
-                <h4 className="text-primary"><strong>DASHBOARD</strong></h4>   
-                
-             
+              <div className="breadcrumb">           
+                <h4 className="text-primary"><strong>DASHBOARD</strong></h4>  
               </div>
             </div>
-        </div>   
-
-        
+        </div>           
              
         
 
        
             <Card
-
-            />
+              setActivePage={this.setActivePage}
+              // setPageTitle={this.setActivePage}
+              // setStakehNumb={this.setActivePage}
+              />
 
 
           
