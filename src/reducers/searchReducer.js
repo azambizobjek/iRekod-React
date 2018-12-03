e43aaf659f2cd23168b348c810378253782042a9
Kardiwangsa <fariz@bizobjek.com>
2018-12-03 10:55:36 +0800

    Update searchReducer.js

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,5 +1,4 @@
 import {BASIC_SEARCH,STAKEH_LIST} from '../actions/types'
-const _ = require('lodash') // Lodash.
 
 
 const initialState={
@@ -16,24 +15,9 @@ export default function(state = initialState, action){
             stakehList:action.payload,
         }
         case BASIC_SEARCH:
-        const {payload} = action
-        const filtered = _.filter(state.stakehList, (x) => _.toLower(x.full_name).includes(_.toLower(payload)))
-        return {...state, filtered}
-        // case TOGGLE_SIDENAV:
-        // return {
-        //     ...state,
-        //     toggleSideNav:action.payload
-        // }
-        // case SIDENAV_CLASS:
-        // return {
-        //     ...state,
-        //     navBarClass:action.payload
-        // }
-        // case ACTIVE_PAGE:
-        // return {
-        //     ...state,
-        //     activePage:action.payload
-        // }
+        return {...state,
+            basicKey:action.payload,
+        }       
         default:
         return state
     }
