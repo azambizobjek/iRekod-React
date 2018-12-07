1eaf6a89ca6b9e7a85b3c67c105b558116c5686e
farishelmy <42462837+farishelmy@users.noreply.github.com>
2018-12-07 17:16:28 +0800

    Update CardView.js

--- src/components/workflow/CardView.js
+++ src/components/workflow/CardView.js
@@ -28,7 +28,7 @@ export default function Workflow({title, markOnSel,taskId, isSel, subject}) {
 
     return (
         <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-        <Box>
+        
            <div className={isSel?"card bg-primary":"card"} onClick={handleClick} >
                <div className="text-center">
                     <img className="img-card mt-4" src={require('../../img/management.svg')} alt="activity"/>
@@ -38,7 +38,7 @@ export default function Workflow({title, markOnSel,taskId, isSel, subject}) {
                    <p className={isSel?"card-title mb-1 font-weight-bold text-truncate text-light":"card-title mb-1 font-weight-bold text-truncate text-muted"}>{decodeURIComponent(title)}</p>
                </div>
            </div>
-           </Box>
+           
        </div>
 
 
